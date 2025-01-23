7fcb1580e48d15ae696f56e30fd14561da138dc4
Matisse Mosher <matissemosher@mail.weber.edu>
2025-01-22 21:10:14 -0700

    final

---
+++ java.js
@@ -0,0 +1,21 @@
+function blossom() {
+  document.getElementById("settings").innerHTML = "Current Setting: Blossom";
+  document.getElementById("pageBody").style = "background-color: #e4a3ad";
+  document.getElementById("footer").style =
+    "background-color:rgb(238, 199, 206)";
+}
+
+function bubbles() {
+  document.getElementById("settings").innerHTML = "Current Setting: Bubbles";
+  document.getElementById("pageBody").style =
+    "background-color:rgb(129, 204, 231)";
+  document.getElementById("footer").style =
+    "background-color:rgb(187, 223, 236)";
+}
+
+function buttercup() {
+  document.getElementById("settings").innerHTML = "Current Setting: Buttercup";
+  document.getElementById("pageBody").style = "background-color: #81CB71";
+  document.getElementById("footer").style =
+    "background-color:rgb(191, 211, 187)";
+}
